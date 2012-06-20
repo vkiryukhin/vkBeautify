795b2896c5aaaea0be6c3a50a2f4c519264d4be8
Vadim Kiryukhin <vkiryukhin@gmail.com>
2012-06-19 21:03:09 -0700

    changed version to 0.98.01.beta

--- vkbeautify.js
+++ vkbeautify.js
@@ -1,7 +1,7 @@
 /**
 * vkBeautify - javascript plugin to pretty-print or minify text in XML, JSON, CSS and SQL formats.
 *  
-* Version - 0.98.00.beta 
+* Version - 0.98.01.beta 
 * Copyright (c) 2012 Vadim Kiryukhin
 * vkiryukhin @ gmail.com
 * http://www.eslinstructor.net/vkbeautify/
